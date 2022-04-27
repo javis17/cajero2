17981b6daed4da6aec07f0d8e1bfde07fc0121d8
javis17 <101935242+javis17@users.noreply.github.com>
2022-04-27 12:39:07 -0500

    Add files via upload

---
+++ cajero1/index.js
@@ -0,0 +1,153 @@
+//DECLARACION DE VARIABLES
+class Billete
+{
+  constructor (valor, cantidad)
+  {
+    this.valor = valor;
+    this.cantidad = cantidad;
+    this.imagen = new Image();
+    this.imagen.src = imagenes[this.valor];
+  }
+  mostrar()
+  {
+    document.body.appendChild(this.imagen);
+  }
+}
+
+var sonidos  =
+{
+  tecla: document.getElementById("tecla_id"),
+  alerta: document.getElementById("alerta_id"),
+  entrega: document.getElementById("entrega_id"),
+}
+
+var imagenes = [];
+imagenes[1000] = "1000.jpg";
+imagenes[500] = "500.jpg";
+imagenes[100] = "100.jpg";
+imagenes[50] = "50.jpg";
+imagenes[20] = "20.jpg";
+imagenes[10] = "10.png";
+
+var caja = [];
+caja.push(new Billete(1000, 10));
+caja.push(new Billete(500, 20));
+caja.push(new Billete(100, 10));
+caja.push(new Billete(50, 200));
+caja.push(new Billete(20, 500));
+caja.push(new Billete(10, 3));
+
+var resultado = document.getElementById("resultado");
+var carga = document.getElementById("carga_id");
+var pantalla = document.getElementById("pantalla_texto");
+var boton_extraccion = document.getElementById("botonExtraccion");
+var papeles;
+var div;
+var dinero;
+var dinero_disponible = 0;
+var entregado = [];
+
+// OTROS EVENTOS Y FUNCIONES
+contador();
+
+boton_extraccion.addEventListener("click", insertarDinero);
+document.addEventListener("keydown", sonido_tecla);
+                                                         
+//DECLARACION DE FUNCIONES
+function sonido_tecla()
+{
+  sonidos.tecla.play();
+}
+
+function contador() //dinero total de  la caja
+{
+  for(v of caja)
+  {
+    dinero_disponible = dinero_disponible + (v.valor * v.cantidad);
+  }
+}
+
+function insertarDinero()
+{
+  entregado.splice(0, entregado.length); //Reseteo el array entregado
+  dinero = parseInt(pantalla.value);
+  compatibilidad();
+}
+
+function compatibilidad() //solo tomará multiplos positivos de 10 y distintos de 0
+{
+  if (dinero > dinero_disponible) // SI NO HAY SUFICIENTE DINERO
+  {
+    sonidos.alerta.play();
+    resultado.innerHTML = "No tenemos tantos billetotes!";
+  } else
+  {
+    if (dinero % 20 == 0 && dinero > 0)
+    {
+      entregarDinero();
+    } else if (dinero == 0)
+    {
+      sonidos.alerta.play();
+      resultado.innerHTML = "Para que vienes al cajero?";
+    } else if (dinero % 20 !== 0 && dinero > 0)
+    {
+      sonidos.alerta.play();
+      resultado.innerHTML = "Inserte solo múltiplos de 20!";
+    } else if (dinero < 0)
+    {
+      sonidos.alerta.play();
+      resultado.innerHTML = "No se pueden insertar valores negativos";
+    }
+  }
+}
+
+function entregarDinero()
+{
+  for(v of caja)
+  {
+    if (dinero > 0)
+    {
+      div = parseInt(dinero / v.valor) //NUMERO DE BILLETES NECESARIOS
+
+      if (div > v.cantidad) //CUANDO NO ALCANZAN LOS BILLETES MAS GRANDES
+      {
+        papeles = v.cantidad;
+        dinero_disponible = dinero_disponible - (v.valor * v.cantidad);
+        dinero = dinero - (v.valor * v.cantidad);
+        entregado.push(new Billete(v.valor, papeles));
+        v.cantidad = 0//SE AGOTAN LOS BILLETES AL TERMINAR
+        check();
+      } else //CUANDO SI ALCANZAN LOS BILLETES MAS GRANDES
+      {
+        papeles = div;
+        dinero_disponible = dinero_disponible - (v.valor * div);
+        dinero = dinero - (v.valor * div);
+        entregado.push(new Billete(v.valor, papeles));
+        v.cantidad = v.cantidad - div;
+        check();
+      }
+    }
+  }
+}
+
+function check() //VERIFICA SI LA SUMA ES VÁLIDA RESPECTO A LOS BILLETES DISPONIBLES
+{
+  if (dinero == 0)//ENTREGA EL DINERO
+  {
+    sonidos.entrega.play();
+    carga.innerHTML += "<br /> Usted retiró: <br />";
+    for(e of entregado)
+    {
+      resultado.innerHTML = "Retire su dinero abajo ↓";
+      for (var i = 0; i < e.cantidad; i++)
+      {
+        carga.innerHTML += "<img src=" + e.imagen.src + " />";
+      }
+    }
+  } else
+  {
+    sonidos.alerta.play();
+    resultado.innerHTML = "No tenemos los billetes para esa suma";
+  }
+}
+
